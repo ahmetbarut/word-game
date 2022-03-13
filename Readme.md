f2125e2bb6f75ed2a01b469539ba6273db6ed2c8
Ahmet Barut <ahmetbarut588@gmail.com>
2022-03-13 03:04:29 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Word Game With Terminal
+
+A fun, simple game that I wrote to learn a foreign language
+
+## Installation
+
+```bash
+    composer global require "ahmetbarut/word-game"
+```
+
---
+++ src/Words.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AhmetBarut\WordGame;
+
+class Words
+{
+    /**
+     * @var array
+     */
+    protected array $words;
+
+    /**
+     * Words constructor.
+     */
+    public function __construct()
+    {
+        $this->parseWords(file_get_contents(__DIR__ . '/words.txt'));
+    }
+
+    /**
+     * @param string $words
+     */
+    public function parseWords($words): void
+    {
+        preg_match_all('/(.*):(.*)/m', $words, $matches, PREG_SET_ORDER, 0);
+
+        foreach ($matches as $match) {
+            $this->words[] = [
+                'en' => $match[1],
+                'tr' => rtrim(trim(str_replace(
+                    ['s.', 'i.', 'zf.', 'z.', 'f.', 'bğ.', 'ed.'],
+                    '',
+                    $match[2]
+                ))),
+            ];
+        }
+    }
+
+    /**
+     * get matching words
+     * @param int $count
+     * @return array
+     */
+    public function getWords(): array
+    {
+        return $this->words;
+    }
+
+    /**
+     * get random word
+     * @param int $count
+     * @return array
+     */
+    public function getRandomWord(?int $count = 1): array
+    {
+        $randomWords = [];
+
+        for ($i = 0; $i < $count; $i++) {
+            $randomWords[] = $this->words[array_rand($this->words)];
+        }
+        return $randomWords;
+    }
+}
